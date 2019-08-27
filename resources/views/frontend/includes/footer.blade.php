e4285f23b0bd06b0a3cb25801aed7be5bc83c9f2
john.glauber <johngna@gmail.com>
2019-08-27 09:19:01 -0300

    area do cliente

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,12 +1,35 @@
 <div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
-    <div class="kt-container  kt-container--fluid ">
-        <div class="kt-footer__copyright">
-            2019&nbsp;&copy;&nbsp;<a href="" target="_blank" class="kt-link">ANIMAX</a>
+    <div class="kt-container  kt-container--fluid " style="padding:0px">
+
+        <div class="kt-portlet kt-portlet--fit kt-portlet--head-lg kt-portlet--head-overlay kt-portlet--height-fluid">
+            <div class="kt-portlet__head kt-portlet__space-x">
+
+
+            </div>
+            <div class="kt-portlet__body">
+                <div class="kt-widget28">
+                    <div class="kt-widget28__visual" style="background-image: url(./assets/media//misc/bg-2.jpg)"></div>
+                    <div class="kt-widget28__wrapper kt-portlet__space-x">
+                        <!-- begin::Nav pills -->
+                        <ul class="nav nav-pills nav-fill kt-portlet__space-x" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="pill" href="#menu11"><span><i class="fa flaticon-pie-chart"></i></span><span>GMI Taxes</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="pill" href="#menu21"><span><i class="fa flaticon-file-1"></i></span><span>IMT Invoice</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="pill" href="#menu31"><span><i class="fa flaticon-clipboard"></i></span><span>Main Notes</span></a>
+                            </li>
+                        </ul>
+                        <!-- end::Nav pills -->
+
+
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="kt-footer__menu">
-            <a href="" target="_blank" class="kt-footer__menu-link kt-link">Sobre</a>
-            <a href="" target="_blank" class="kt-footer__menu-link kt-link">Time</a>
-            <a href="" target="_blank" class="kt-footer__menu-link kt-link">Contato</a>
-        </div>
+
+
     </div>
 </div>
