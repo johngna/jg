16436dc7ab8def8d1bf47303754095be3710d601
john.glauber <johngna@gmail.com>
2019-08-27 09:35:26 -0300

    area do cliente

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -5,13 +5,13 @@
                         <!-- begin::Nav pills -->
                         <ul class="nav nav-pills nav-fill kt-portlet__space-x" role="tablist" style="margin-bottom:0px">
                             <li class="nav-item">
-                                <a class="nav-link active" data-toggle="pill" href="#menu11"><span><i class="fa flaticon-pie-chart"></i><br></span><span> MEUS PETS</span></a>
+                                <a class="nav-link" data-toggle="pill" href="#menu11"><span><i class="fa fa-paw"></i><br></span><span> MEUS PETS</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="pill" href="#menu21"><span><i class="fa flaticon-file-1"></i><br></span><span> FINANCEIRO</span></a>
+                                <a class="nav-link" data-toggle="pill" href="#menu21"><span><i class="fa flaticon-file-1"></i><br></span><span> ASSOCIAÇÃO</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="pill" href="#menu31"><span><i class="fa flaticon-clipboard"></i><br></span><span> ASSOCIAÇÃO</span></a>
+                                <a class="nav-link  active" data-toggle="pill" href="#menu31"><span><i class="fa flaticon-clipboard"></i><br></span><span> NOTÍCIAS</span></a>
                             </li>
                         </ul>
                         <!-- end::Nav pills -->
