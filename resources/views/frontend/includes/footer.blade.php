009816edb278ad5db052f89ae05382b7363a7944
john.glauber <johngna@gmail.com>
2019-08-27 09:23:27 -0300

    area do cliente

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,34 +1,22 @@
-<div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
+<div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer" style="padding:0px">
     <div class="kt-container  kt-container--fluid " style="padding:0px">
 
-        <div class="kt-portlet kt-portlet--fit kt-portlet--head-lg kt-portlet--head-overlay kt-portlet--height-fluid">
-            <div class="kt-portlet__head kt-portlet__space-x">
 
-
-            </div>
-            <div class="kt-portlet__body">
-                <div class="kt-widget28">
-                    <div class="kt-widget28__visual" style="background-image: url(./assets/media//misc/bg-2.jpg)"></div>
-                    <div class="kt-widget28__wrapper kt-portlet__space-x">
                         <!-- begin::Nav pills -->
-                        <ul class="nav nav-pills nav-fill kt-portlet__space-x" role="tablist">
+                        <ul class="nav nav-pills nav-fill kt-portlet__space-x" role="tablist" style="margin-bottom:0px">
                             <li class="nav-item">
-                                <a class="nav-link active" data-toggle="pill" href="#menu11"><span><i class="fa flaticon-pie-chart"></i></span><span>GMI Taxes</span></a>
+                                <a class="nav-link active" data-toggle="pill" href="#menu11"><span><i class="fa flaticon-pie-chart"></i><br></span><span> PETS</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="pill" href="#menu21"><span><i class="fa flaticon-file-1"></i></span><span>IMT Invoice</span></a>
+                                <a class="nav-link" data-toggle="pill" href="#menu21"><span><i class="fa flaticon-file-1"></i><br></span><span> FINANCEIRO</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="pill" href="#menu31"><span><i class="fa flaticon-clipboard"></i></span><span>Main Notes</span></a>
+                                <a class="nav-link" data-toggle="pill" href="#menu31"><span><i class="fa flaticon-clipboard"></i><br></span><span> ASSOCIAÇÃO</span></a>
                             </li>
                         </ul>
                         <!-- end::Nav pills -->
 
 
-                    </div>
-                </div>
-            </div>
-        </div>
 
 
     </div>
